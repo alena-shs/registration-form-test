1f9e6ea516d629cb9f30f628262921d58c0ad38b
Alena Shomanova <alenashomanova@gmail.com>
2023-04-09 18:10:28 -0400

    Adds a remote feature for tests

---
+++ src/test/java/tests/FormTestSelenoid.java
@@ -0,0 +1,53 @@
+package tests;
+
+import objects.RegistrationPage;
+import objects.components.RegistrationResultsModal;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+import java.text.DecimalFormat;
+
+import static tests.TestData.*;
+
+@Tags({
+        @Tag("JavaFaker"),
+        @Tag("simple")})
+public class FormTestSelenoid extends TestBaseSelenoid {
+    RegistrationPage registrationPage = new RegistrationPage();
+    RegistrationResultsModal registrationResultsModal = new RegistrationResultsModal();
+
+    @Test
+    void studentRegistrationFormPositiveTest() {
+
+        registrationPage
+                .openPage()
+                .removeBanners()
+                .setFirstName(name)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender(gender)
+                .setMobileNumber(mobileNumber)
+                .setBirthDate(birthDay, birthMonth, birthYear)
+                .setSubject(subject)
+                .setHobby(hobby)
+                .setPicture(filename)
+                .setAddress(address)
+                .setState(state)
+                .setCity(city)
+                .submitForm();
+
+        registrationResultsModal
+                .verifyModalAppears()
+                .verifyModalContents("Student Name", (name + " " + lastName))
+                .verifyModalContents("Student Email", email)
+                .verifyModalContents("Gender", gender)
+                .verifyModalContents("Mobile", mobileNumber)
+                .verifyModalContents("Date of Birth", ((new DecimalFormat("00").format(birthDay)) + " " + birthMonth + "," + birthYear))
+                .verifyModalContents("Subjects", subject)
+                .verifyModalContents("Hobbies", hobby)
+                .verifyModalContents("Picture", filename)
+                .verifyModalContents("Address", address)
+                .verifyModalContents("State and City", (state + " " + city));
+    }
+}
---
+++ src/test/java/tests/TestBaseSelenoid.java
@@ -0,0 +1,30 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+@Tag("Remote")
+public class TestBaseSelenoid {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+}
