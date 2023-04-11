8dc4232baf005c4c4ae35409888c807651e547a8
Alena Shomanova <alenashomanova@gmail.com>
2023-04-11 10:39:13 -0400

    Adds properties for Jenkins

--- src/test/java/tests/TestBaseSelenoid.java
+++ src/test/java/tests/TestBaseSelenoid.java
@@ -15,18 +15,12 @@ public class TestBaseSelenoid {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
 
-        String browserName = System.getProperty("browser", "firefox");
-        System.out.println(browserName);
-        Configuration.browser =  browserName;
-
-        String browserVersion = System.getProperty("browser_version", "97.0");
-        System.out.println(browserVersion);
-        Configuration.browserVersion = browserVersion;
-
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser =  System.getProperty("browser", "firefox");
+        Configuration.browserVersion = System.getProperty("browser_version", "97.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
